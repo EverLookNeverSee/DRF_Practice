d9823187c9b298efa9f9ca4a28d6a576ecfba0e4
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 20:50:41 +0330

    Changing topic serializer

--- topics/serializers.py
+++ topics/serializers.py
@@ -3,9 +3,7 @@ from topics.models import Topic
 
 
 class TopicSerializer(serializers.ModelSerializer):
-    likes = serializers.IntegerField(read_only=True)
-    dislikes = serializers.IntegerField(read_only=True)
-
     class Meta:
         model = Topic
         fields = ("title", "description", "likes", "dislikes")
+        read_only_fields = ["likes", "dislikes"]
