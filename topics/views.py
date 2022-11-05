75651b8bdd9adb7c84c61397e5438d78035e563c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 20:50:57 +0330

    Changing queryset

--- topics/views.py
+++ topics/views.py
@@ -1,12 +1,8 @@
-from django.db.models import Count, Q
 from rest_framework.generics import ListCreateAPIView
 from .models import Topic, Vote
 from .serializers import TopicSerializer
 
 
 class TopicListCreateAPIView(ListCreateAPIView):
-    queryset = Topic.objects.all().annotate(
-        likes=Count("votes", filter=Q(votes__vote=Vote.VoteChoice.like)),
-        dislikes=Count("votes", filter=Q(votes__vote=Vote.VoteChoice.dislike))
-    )
+    queryset = Topic.objects.all()
     serializer_class = TopicSerializer
