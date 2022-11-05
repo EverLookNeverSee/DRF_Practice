d954bbd4a45e1bced146eb6710d6af8c4cbbf84f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 21:10:24 +0330

    Defining votes url path

--- topics/urls.py
+++ topics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from topics.views import TopicListCreateAPIView
+from topics.views import TopicListCreateAPIView, VoteCreateAPIView
 
 
 urlpatterns = [
     path("", TopicListCreateAPIView.as_view(), name="topics"),
+    path("votes/", VoteCreateAPIView.as_view(), name="vote-create")
 ]
