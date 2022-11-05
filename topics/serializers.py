31dd2cb0f5d14e7ffd25b44f9842ba8f1d6e3042
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 20:18:59 +0330

    Implementing topic serializer

---
+++ topics/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from topics.models import Topic
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    likes = serializers.IntegerField(read_only=True)
+    dislikes = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Topic
+        fields = ("title", "description", "likes", "dislikes")
