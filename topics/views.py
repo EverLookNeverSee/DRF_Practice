9f100a5829e91d90614adcfc7a76a8b13ed7bc84
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 21:10:03 +0330

    Implementing vote create api view

--- topics/views.py
+++ topics/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, CreateAPIView
 from .models import Topic, Vote
-from .serializers import TopicSerializer
+from .serializers import TopicSerializer, VoteSerializer
 
 
 class TopicListCreateAPIView(ListCreateAPIView):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+
+class VoteCreateAPIView(CreateAPIView):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
