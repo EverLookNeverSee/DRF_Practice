4f11ed33684d954266f11153b7012c4e96be05fc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 20:19:37 +0330

    Implementing TopicListCreateAPIView

--- topics/views.py
+++ topics/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Count, Q
+from rest_framework.generics import ListCreateAPIView
+from .models import Topic, Vote
+from .serializers import TopicSerializer
 
-# Create your views here.
+
+class TopicListCreateAPIView(ListCreateAPIView):
+    queryset = Topic.objects.all().annotate(
+        likes=Count("votes", filter=Q(votes__vote=Vote.VoteChoice.like)),
+        dislikes=Count("votes", filter=Q(votes__vote=Vote.VoteChoice.dislike))
+    )
+    serializer_class = TopicSerializer
