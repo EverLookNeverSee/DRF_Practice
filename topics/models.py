60b7090112ffe30af571f772c4298c42da2c406f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-05 20:50:19 +0330

    Optimizing model and its queryset

--- topics/models.py
+++ topics/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import F
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
 
@@ -6,8 +9,11 @@ class Topic(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
     class Meta:
+        ordering = ("-likes",)
         verbose_name = _("Topic")
         verbose_name_plural = _("topics")
 
@@ -23,3 +29,11 @@ class Vote(models.Model):
     class Meta:
         verbose_name = _("Vote")
         verbose_name_plural = _("Votes")
+
+
+@receiver(post_save, sender=Vote)
+def update_votes(sender, instance, **kwargs):
+    if instance.vote == Vote.VoteChoice.like:
+        Topic.objects.filter(id=instance.topic.id).update(likes=F("likes") + 1)
+    else:
+        Topic.objects.filter(id=instance.topic.id).update(dislikes=F("dislikes") + 1)
